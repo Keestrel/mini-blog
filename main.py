194b546e46a51472973ba0f11690724acbd0a2ca
Rostrunn <iv.klyuev2006@gmail.com>
2026-09-21 15:45:28 +0300

    added front
    Добвален нейро-фронтенд0. Создан 3 контейнер под фронтенд

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.routers.auth import router as auth_router
 from api.routers.posts import router as posts_router
 
@@ -21,5 +23,12 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
 app.include_router(posts_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
